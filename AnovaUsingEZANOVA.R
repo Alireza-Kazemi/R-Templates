5f1218af4facca6771ee23d46e3dd3c637df13d2
Alireza-Kazemi <62960169+Alireza-Kazemi@users.noreply.github.com>
2022-07-12 03:45:25 -0700

    Update AnovaUsingEZANOVA.R

--- AnovaUsingEZANOVA.R
+++ AnovaUsingEZANOVA.R
@@ -7,3 +7,19 @@ results$pareta=results$SSn/(results$SSn+results$SSd)
 is.num=sapply(results, is.numeric)
 results[is.num] =lapply(results[is.num], round, 3)
 results
+
+
+
+#################### With missing values ##########
+
+Sdat = reshape2::dcast(FeatDat,Band+Stim+Health ~ Run, value.var="value")
+Sdat = melt(Sdat, id.vars = c("Health","Stim","Band"),
+            variable.name = "Run")
+Sdat$value[is.na(Sdat$value)]=0
+
+results=as.data.frame(ezANOVA(data=Sdat, dv="value", wid=.("Run"), within=.("Band"),
+                              between = c("Stim","Health"), type=3,detailed=T)$ANOVA)
+results$pareta=results$SSn/(results$SSn+results$SSd)
+is.num=sapply(results, is.numeric)
+results[is.num] =lapply(results[is.num], round, 3)
+results
