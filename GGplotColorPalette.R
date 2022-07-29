b2f43a68d30627779ad0bad7f228d85e5e3db621
Alireza-Kazemi <62960169+Alireza-Kazemi@users.noreply.github.com>
2022-07-28 18:48:29 -0700

    Update GGplotColorPalette.R

--- GGplotColorPalette.R
+++ GGplotColorPalette.R
@@ -1,5 +1,19 @@
+
+library("RColorBrewer")
+display.brewer.all()
+  
+display.brewer.pal(n = 8, name = 'RdBu')
+
+brewer.pal(n = 8, name = "RdBu")
+
+
+
 ggplot(df, aes(x=cond, y=yval, fill=cond)) + geom_bar(stat="identity") +
   scale_fill_brewer(palette="Spectral")
 
 ggplot(df, aes(x=cond, y=yval, fill=cond)) + geom_bar(stat="identity") + 
   scale_fill_manual(values=c("#CC6666", "#9999CC", "#66CC99"))
+  
+  
+  
+  
