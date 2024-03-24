cc6a8a4a021567fa16427ac5d0636923430f5498
Alireza-Kazemi <62960169+Alireza-Kazemi@users.noreply.github.com>
2024-03-23 17:23:14 -0700

    update

--- Long_Wide_Coversion.R
+++ Long_Wide_Coversion.R
@@ -6,3 +6,9 @@ dat$Feature = gsub("Feature_","",dat$Feature, ignore.case = T)
 temp = as.data.frame(summarise(group_by(dat,SID,Health,Medication,Stim,Channel,Trial,Vigor,Feature),N=n()))
 
 dat = reshape2::dcast(dat,SID+Health+Medication+Stim+Channel+Trial+Vigor+OddEven ~ Feature, value.var="value")
+
+
+A1 = pivot_wider(A, 
+                id_cols = c("ID","Ntot"), 
+                names_from = c("ConditionName", "Target2"),
+                values_from = c("N","ACC"))
